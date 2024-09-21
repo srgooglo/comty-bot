ccbcb77381ecfb94f89dae428defd1db52f3d112
SrGooglo <srgooglo@ragestudio.net>
2024-09-21 20:10:35 +0000

    improve envets handlers

--- src/features/UserLevels/index.js
+++ src/features/UserLevels/index.js
@@ -1,158 +1,334 @@
-import { Events, EmbedBuilder } from "discord.js";
+import { Events } from "discord.js"
 
-import Feature from "@classes/Feature";
+import Feature from "@classes/Feature"
 
-import GuildLevelsConfigCache from "@classes/GuildLevelsConfigCache";
-import UserLevelManager from "@classes/UserLevelsManager";
+import GuildLevelsConfigCache from "@classes/GuildLevelsConfigCache"
+import UserLevelManager from "@classes/UserLevelsManager"
 
-import buildLevelUpEmbed from "./embedds/level_up";
+import buildLevelUpEmbed from "./embeds/level_up"
+import buildLevelInfoEmbed from "./embeds/level_info"
+import buildLevelTopEmbed from "./embeds/level_top"
+import buildLevelListEmbed from "./embeds/level_list"
 
-import getTargetLevelFromPoints from "./utils/getTargetLevelFromPoints";
-import createLevelInfoEmbed from "./utils/createLevelInfoEmbed";
-import hasCooldown from "./utils/hasCooldown";
+import getTargetLevelFromPoints from "./utils/getTargetLevelFromPoints"
+import hasCooldown from "./utils/hasCooldown"
 
-import DEFAULT_FEATURE_CONFIG from "./defaultConfig";
+import DEFAULT_FEATURE_CONFIG from "./defaultConfig"
 
 export default class UserLevelsFeature extends Feature {
-  static featureKey = "user_levels";
+	static featureKey = "user_levels"
 
-  registerEvents = {
-    [Events.MessageCreate]: this.handleOnMessage,
-    [Events.VoiceStateUpdate]: this.handleOnVoiceStateUpdate,
-  };
+	registerEvents = {
+		[Events.MessageCreate]: this.handleOnMessage,
+		[Events.VoiceStateUpdate]: this.handleOnVoiceStateUpdate,
+		[Events.GuildMemberAdd]: this.handleOnGuildMemberAdd,
+	}
 
-  registerCommands = [
-    {
-      name: "level",
-      description: "Get your level",
-      fn: this.handleLevelCommand,
-    },
-  ];
+	registerCommands = [
+		{
+			name: "level",
+			options: [
+				{
+					type: "string",
+					name: "action",
+					description: "Action to perform",
+					choices: [
+						{
+							name: "top",
+							value: "top",
+							description: "Get the top 10 users in the leaderboard",
+						},
+						{
+							name: "sync",
+							value: "sync",
+							description: "Sync your level in case of a desync",
+						},
+						{
+							name: "list",
+							value: "list",
+							description: "Get the list of available levels",
+						}
+					],
+				}
+			],
+			description: "Manage your server level",
+			fn: async (interaction) => {
+				const action = interaction.options.getString("action") ?? "default"
 
-  userLevelManager = new UserLevelManager();
-  guildsConfigCache = new GuildLevelsConfigCache({
-    defaultConfig: DEFAULT_FEATURE_CONFIG,
-  });
+				if (!this.commands[action]) {
+					return await interaction.reply("Unknown action")
+				}
 
-  async handleLevelCommand(interaction) {
-    const guildLevelsConfig = await this.guildsConfigCache.get(
-      interaction.guild.id,
-    );
-    const levelObj = await this.userLevelManager.getObj(
-      interaction.user.id,
-      interaction.guild.id,
-    );
+				return await this.commands[action](interaction)
+			},
+		},
+	]
 
-    await interaction.reply({
-      embeds: [createLevelInfoEmbed(null, guildLevelsConfig, levelObj)],
-      ephemeral: true,
-    });
-  }
+	userLevelManager = new UserLevelManager()
+	guildsConfigCache = new GuildLevelsConfigCache({
+		defaultConfig: DEFAULT_FEATURE_CONFIG,
+	})
 
-  onLevelUp = async (user_id, guild_id, currentLevelObj) => {
-    const guildLevelsConfig = await this.guildsConfigCache.get(guild_id);
+	getUserRoles = async (user_id, guild_id) => {
+		const guild = await this.bot.client.guilds.fetch(guild_id)
+		const member = await guild.members.fetch(user_id)
+		const roles = member.roles.cache
 
-    // if notifyChannelId exist, post a message in the channel
-    if (guildLevelsConfig.notifyChannelId) {
-      const channel = await this.bot.client.channels.fetch(
-        guildLevelsConfig.notifyChannelId,
-      );
+		return roles
+	}
 
-      const userData = await this.bot.client.users.fetch(user_id);
+	getRoleData = async (role_id, guild_id) => {
+		const guild = await this.bot.client.guilds.fetch(guild_id)
+		const role = await guild.roles.fetch(role_id)
 
-      let embed = buildLevelUpEmbed(guildLevelsConfig, currentLevelObj);
-      embed = createLevelInfoEmbed(embed, guildLevelsConfig, currentLevelObj);
+		return role
+	}
 
-      embed = embed.setImage(
-        `https://cdn.discordapp.com/avatars/${user_id}/${userData.avatar}.png?size=256`,
-      );
+	onLevelUp = async (user_id, guild_id, currentLevelObj) => {
+		const guildLevelsConfig = await this.guildsConfigCache.get(guild_id)
 
-      await channel.send({
-        embeds: [embed],
-        allowedMentions: {
-          users: [user_id],
-        },
-      });
-    }
-  };
+		// if notifyChannelId exist, post a message in the channel
+		if (guildLevelsConfig.notifyChannelId) {
+			const channel = await this.bot.client.channels.fetch(
+				guildLevelsConfig.notifyChannelId,
+			)
+			const userData = await this.bot.client.users.fetch(user_id)
 
-  handleNextLevel = async (user_id, guild_id) => {
-    const guildLevelsConfig = await this.guildsConfigCache.get(guild_id);
-    const currentLevelObj = await this.userLevelManager.getObj(
-      user_id,
-      guild_id,
-    );
+			const userLevelRank = await this.userLevelManager.getUserLevelIndex(user_id, guild_id)
 
-    let targetLevel = getTargetLevelFromPoints(
-      currentLevelObj.points,
-      guildLevelsConfig.levels,
-    );
+			currentLevelObj.rank = userLevelRank
 
-    if (currentLevelObj.level !== targetLevel.level) {
-      await this.userLevelManager.updateLevel(
-        user_id,
-        guild_id,
-        targetLevel.level,
-      );
+			let embed = buildLevelUpEmbed(guildLevelsConfig, currentLevelObj, userData)
 
-      await this.onLevelUp(user_id, guild_id, {
-        ...currentLevelObj,
-        level: targetLevel.level,
-      });
-    }
-  };
+			await channel.send({
+				content: `<@${user_id}>`,
+				embeds: [embed],
+				allowedMentions: { users: [user_id] },
+			})
+		}
+	}
 
-  async handleOnMessage(message) {
-    let guildLevelsConfig = await this.guildsConfigCache.get(message.guild.id);
+	handleNextLevel = async (user_id, guild_id) => {
+		const userRoles = await this.getUserRoles(user_id, guild_id)
+		const guildLevelsConfig = await this.guildsConfigCache.get(guild_id)
+		const currentLevelObj = await this.userLevelManager.getObj(
+			user_id,
+			guild_id,
+		)
+		const guild = await this.bot.client.guilds.fetch(guild_id)
+		const member = await guild.members.fetch(user_id)
 
-    const earning = guildLevelsConfig.earnings["message"];
+		let targetLevel = getTargetLevelFromPoints(
+			currentLevelObj.points,
+			guildLevelsConfig.levels,
+		)
 
-    // just ingore if disabled
-    if (!earning.enabled) {
-      return false;
-    }
+		if (currentLevelObj.level !== targetLevel.level) {
+			await this.userLevelManager.updateLevel(
+				user_id,
+				guild_id,
+				targetLevel.level,
+			)
 
-    const currentLevel = await this.userLevelManager.getObj(
-      message.author.id,
-      message.guild.id,
-    );
+			await this.onLevelUp(user_id, guild_id, {
+				...currentLevelObj,
+				level: targetLevel.level,
+			})
+		}
 
-    // check the cooldown
-    if (hasCooldown(currentLevel.lastInteraction, earning)) {
-      return false;
-    }
+		// get target role
+		const targetRole = guildLevelsConfig.levels.find((level) => level.level === currentLevelObj.level).role
+		const excludedRoles = guildLevelsConfig.levels.filter((level) => level.level !== currentLevelObj.level).map(level => level.role)
 
-    const nextPoints = currentLevel.points + earning.points;
+		// remove excluded roles
+		for await (const roleId of excludedRoles) {
+			if (userRoles.has(roleId)) {
+				await member.roles.remove(roleId)
+			}
+		}
 
-    this.console.info(
-      `[${message.author.username}] Points update due a message [${currentLevel.points}]+${earning.points} = [${nextPoints}]`,
-    );
+		// check if the user has the target role, if not, give him the role
+		if (!userRoles.has(targetRole.id)) {
+			await member.roles.add(targetRole)
+		}
+	}
 
-    await this.userLevelManager.updatePoints(
-      message.author.id,
-      message.guild.id,
-      nextPoints,
-    );
-    await this.userLevelManager.updateLastInteraction(
-      message.author.id,
-      message.guild.id,
-      {
-        date: new Date(),
-        type: "message",
-        data: message.content,
-      },
-    );
+	async handleOnMessage(message) {
+		// ignore if the message is from a bot
+		if (message.author.bot) {
+			return false
+		}
 
-    await this.handleNextLevel(message.author.id, message.guild.id);
+		let guildLevelsConfig = await this.guildsConfigCache.get(message.guild.id)
 
-    return null;
-  }
+		// check if the channel is blacklisted
+		if (Array.isArray(guildLevelsConfig.blacklistChannels)) {
+			if (guildLevelsConfig.blacklistChannels.includes(message.channel.id)) {
+				this.console.info(
+					`[${message.author.username}] Message ignored because the channel is blacklisted`,
+				)
 
-  async handleOnVoiceStateUpdate(state) {
-    console.time("handleOnVoiceStateUpdate:");
+				return false
+			}
+		}
 
-    //this.console.info(`User [${state.member.user.username}] changed voice state`)
+		const earning = guildLevelsConfig.earnings["message"]
 
-    console.timeEnd("handleOnVoiceStateUpdate:");
-  }
+		// just ingore if disabled
+		if (!earning.enabled) {
+			return false
+		}
+
+		const currentUserLevel = await this.userLevelManager.getObj(
+			message.author.id,
+			message.guild.id,
+		)
+
+		// check the cooldown
+		if (hasCooldown(currentUserLevel.lastMessageInteraction, earning)) {
+			return false
+		}
+
+		const nextPoints = currentUserLevel.points + earning.points
+
+		this.console.info(
+			`[${message.author.username}] Points update due a message [${currentUserLevel.points}]+${earning.points} = [${nextPoints}]`,
+		)
+
+		await this.userLevelManager.updatePoints(
+			message.author.id,
+			message.guild.id,
+			nextPoints,
+		)
+		await this.userLevelManager.updateMessageLastInteraction(
+			message.author.id,
+			message.guild.id,
+			{
+				date: new Date(),
+				type: "message",
+				data: message.content,
+			},
+		)
+
+		await this.handleNextLevel(message.author.id, message.guild.id)
+
+		return null
+	}
+
+	async handleOnVoiceStateUpdate(state) {
+		// TODO: Implement
+		return false
+
+		console.log(state)
+
+		let updateType = null
+
+		const { lastVoiceInteraction } = await this.userLevelManager.getObj(
+			state.userID,
+			state.guildID,
+		)
+
+		// check if the user has been joined or leaved a channel
+		if (state.channelID) {
+			if (!lastVoiceInteraction) {
+				return updateType = "other"
+			}
+
+			// it means has changed the state or leaved
+			// check if user left a channel or is just a update
+			if (lastVoiceInteraction && lastVoiceInteraction.channel_id !== state.channelID) {
+				updateType = "left"
+			}
+
+			//  check if muted or deafened
+			if (state.mute || state.deaf) {
+				updateType = "muted"
+			}
+		} else {
+			// it means has joined a channel 
+			updateType = "joined"
+		}
+
+		await this.userLevelManager.updateVoiceStateLastInteraction(
+			state.userID,
+			state.guildID,
+			{
+				date: new Date(),
+				type: updateType,
+				channel_id: state.channelID,
+			},
+		)
+	}
+
+	async handleOnGuildMemberAdd(member) {
+		await this.handleNextLevel(member.user.id, member.guild.id)
+	}
+
+	// COMMANDS HANDLERS
+	commands = {
+		default: async (interaction) => {
+			const action = interaction.options.getString("action")
+
+			if (action === "sync") {
+				return this.handleLevelStateSyncCommand(interaction)
+			}
+
+			if (action === "top") {
+				return this.handleLevelTopCommand(interaction)
+			}
+
+			if (action === "list") {
+				return this.handleLevelListCommand(interaction)
+			}
+
+			const guildLevelsConfig = await this.guildsConfigCache.get(
+				interaction.guild.id,
+			)
+			const levelObj = await this.userLevelManager.getObj(
+				interaction.user.id,
+				interaction.guild.id,
+			)
+
+			await interaction.reply({
+				embeds: [buildLevelInfoEmbed(null, guildLevelsConfig, levelObj, interaction.user)],
+				ephemeral: true,
+			})
+		},
+		"sync": async (interaction) => {
+			await this.handleNextLevel(interaction.user.id, interaction.guild.id)
+
+			await interaction.reply({
+				content: "Your level state has been synced",
+				ephemeral: true,
+			})
+		},
+		"top": async (interaction) => {
+			let topUserLevels = await this.userLevelManager.getTopUserLevels(interaction.guild.id)
+
+			topUserLevels = topUserLevels.map(async (userLevel) => {
+				userLevel.user = await this.bot.client.users.cache.get(userLevel.user_id)
+
+				return userLevel
+			})
+
+			topUserLevels = await Promise.all(topUserLevels)
+
+			const embed = buildLevelTopEmbed(topUserLevels, null)
+
+			await interaction.reply({
+				embeds: [embed],
+				ephemeral: true,
+			})
+		},
+		"list": async (interaction) => {
+			const guildLevelsConfig = await this.guildsConfigCache.get(interaction.guild.id)
+
+			const embed = buildLevelListEmbed(guildLevelsConfig, null)
+
+			await interaction.reply({
+				embeds: [embed],
+				ephemeral: true,
+			})
+		}
+	}
 }
