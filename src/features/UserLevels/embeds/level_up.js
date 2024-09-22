9653d5843cce3e1302eb31882f6029c52a243ac2
SrGooglo <srgooglo@ragestudio.net>
2024-09-22 22:01:37 +0000

    remove bold

--- src/features/UserLevels/embeds/level_up.js
+++ src/features/UserLevels/embeds/level_up.js
@@ -15,12 +15,12 @@ export default (guildLevelsConfig, userLevelObj, userData, embed) => {
 	embed = embed
 		.setColor("#FF6064")
 		.setAuthor({
-			name: `¡Congratulations, you are now level **${userLevelObj.level}**!`,
+			name: `¡Congratulations, you are now level ${userLevelObj.level}!`,
 			iconURL: "https://media.discordapp.net/attachments/1006929586573021276/1178126728946798643/ezgif.com-apng-to-gif.gif?ex=66f00a44&is=66eeb8c4&hm=5260a7f443b1737e16b0a3c4b47d1bf292a1d11e03828d8f3f8271092126d20b&",
 		})
 		.setDescription(
 			`<@&${previusLevelConfig.role}> **→** <@&${levelConfig.role}>\n
-			You are the **#${userLevelObj.rank + 1}** in the leaderboard\n
+			You are now the **#${userLevelObj.rank + 1}** in the leaderboard\n
 			`,
 		)
 		.setThumbnail(`https://cdn.discordapp.com/avatars/${userLevelObj.user_id}/${userData.avatar}.png?size=256`)
