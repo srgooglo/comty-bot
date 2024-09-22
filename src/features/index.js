1aa63710f7ecd85dd4d8ed816fa13086a304e9b4
SrGooglo <srgooglo@ragestudio.net>
2024-09-22 00:01:57 +0000

    update exports

--- src/features/index.js
+++ src/features/index.js
@@ -1,9 +1,11 @@
 import { default as WelcomeMessage } from "./WelcomeMessage"
 import { default as UserLevels } from "./UserLevels"
+import { default as AutoLeave } from "./AutoLeave"
 import { default as Audio } from "./Audio"
 
 export default {
     WelcomeMessage,
     UserLevels,
+    AutoLeave,
     Audio
 }
