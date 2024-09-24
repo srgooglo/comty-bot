2f5293832576a229d6afc85bc92ac1d4b73a4166
SrGooglo <srgooglo@ragestudio.net>
2024-09-24 19:54:21 +0000

    fix early return if `dir` not exist

--- src/utils/loadCustomGuildCommands.js
+++ src/utils/loadCustomGuildCommands.js
@@ -5,7 +5,7 @@ import path from "node:path"
 import fs from "node:fs"
 
 export default async (dir, guildsCommandsCollection) => {
-    if (!fs.statSync(dir).isDirectory()) {
+    if (!fs.existsSync(dir)) {
         return guildsCommandsCollection
     }
 
